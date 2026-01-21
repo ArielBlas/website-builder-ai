5879127a46e05707800f1aa27c187127e5b0794e
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-01-20 23:04:27 -0300

    feat: header component

---
+++ app/_components/Header.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+type Props = {};
+
+const MenuOptions = [
+  {
+    name: "Pricing",
+    path: "/pricing",
+  },
+  {
+    name: "Contact Us",
+    path: "/contact-us",
+  },
+];
+
+const Header = (props: Props) => {
+  return (
+    <div className="flex items-center justify-between p-4 shadow">
+      {/* Logo  X */}
+      <div className="flex gap-2 items-center">
+        <Image src="/logo.svg" alt="Logo" width={35} height={35} />
+        <h2 className="font-bold text-xl">Website Builder AI</h2>
+      </div>
+      {/* Menu Optinos */}
+      <div className="flex gap-3">
+        {MenuOptions.map((menu, index) => (
+          <Button variant={"ghost"} key={index}>
+            {menu.name}
+          </Button>
+        ))}
+      </div>
+      {/* Get Started Button */}
+      <div>
+        <Button>
+          Get Started <ArrowRight />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
