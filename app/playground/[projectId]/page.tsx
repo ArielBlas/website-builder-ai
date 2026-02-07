3621827a88cd6c8e3ffee9257f3a4264fb003369
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-02-06 23:32:50 -0300

    feat: frames api

--- app/playground/[projectId]/page.tsx
+++ app/playground/[projectId]/page.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PlaygroundHeader from "../_components/PlaygroundHeader";
 import ChatSection from "../_components/ChatSection";
 import WebsiteDesign from "../_components/WebsiteDesign";
 import ElementSettingSection from "../_components/ElementSettingSection";
 import { useParams, useSearchParams } from "next/navigation";
+import axios from "axios";
 
 type Props = {};
 
@@ -11,6 +12,17 @@ const Playground = (props: Props) => {
   const { projectId } = useParams();
   const params = useSearchParams();
   const frameId = params.get("frameId");
+
+  useEffect(() => {
+    if (frameId) GetFrameDetails();
+  }, [frameId]);
+
+  const GetFrameDetails = async () => {
+    const result = await axios.get(
+      "/api/frames?frameId=" + frameId + "&projectId=" + projectId,
+    );
+    console.log(result.data);
+  };
 
   return (
     <div>
