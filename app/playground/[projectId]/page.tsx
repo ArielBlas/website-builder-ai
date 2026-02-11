327ae90b53c180a9a64ef4c493206e757f7c215a
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-02-10 23:43:07 -0300

    feat: playground prompt

--- app/playground/[projectId]/page.tsx
+++ app/playground/[projectId]/page.tsx
@@ -20,6 +20,37 @@ export type Messages = {
   content: string;
 };
 
+const Prompt = `userInput: {userInput}
+Based on the user input, generate a complete HTML Tailwind CSS code using Flowbite UI componentes.
+Use a modern design with blue as the primary color theme. Do not add HTML head or title tag, just body make it fulll responsive.
+Requirements:
+- All primary components must match the theme color.
+- Add proper padding and margin for each element.
+- Components should not be connected to one onther; each element should be independent
+- Design must be fully responsive for all screen sizes.
+- Use placeholders for all images for light mode: 
+https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg and for dark mode use:
+https://www.cibaky.com/wp-content/uploads/2015/12/placeholder-3.jpg
+For image, add alt tag with image prompt for that image
+- Do no include broken links
+- Library already install so do not installed or add in script
+- Header menu options should be spread out and not connected.
+use the following component where appropriate:
+- fa fa icons
+- **Flowbite** for UI copmonents like buttons, modals, forms, tables, tabs and alerts, cards, dialog, dropdown, etc
+- Chart.js for charts & graphs
+- Swiper.js for sliders/carousels
+- tooltip & Popover library (Tippy.js)
+
+Additional requirements:
+- Ensure proper spacing, aligment, and hierarchy for all elements.
+- Include interactive components like modals, dropdowns, and accordions where suitable.
+- Ensure charts are visually appealing and match the theme color.
+- Do not add any extra text before or after the HTML code.
+- Output a complete, ready-to-use HTML page. Do not give any raw text before start and end pont the ai response
+
+`;
+
 const Playground = (props: Props) => {
   const { projectId } = useParams();
   const params = useSearchParams();
@@ -50,7 +81,10 @@ const Playground = (props: Props) => {
     const result = await fetch("/api/ai-model", {
       method: "POST",
       body: JSON.stringify({
-        messages: [...messages, { role: "user", content: message }],
+        messages: [
+          ...messages,
+          { role: "user", content: Prompt?.replace("{userInput}", message) },
+        ],
       }),
     });
 
