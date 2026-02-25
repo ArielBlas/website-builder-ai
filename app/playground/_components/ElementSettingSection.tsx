2894c348d24edf830f8633c4dd4e85d4ada84ee4
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-02-24 23:53:44 -0300

    feat: ElementSettingSection color

--- app/playground/_components/ElementSettingSection.tsx
+++ app/playground/_components/ElementSettingSection.tsx
@@ -15,6 +15,11 @@ type Props = {
 };
 
 const ElementSettingSection = ({ selectedEl, clearSelection }: Props) => {
+  const applyStyle = (property: string, value: string) => {
+    if (selectedEl) {
+      selectedEl.style[property as any] = value;
+    }
+  };
   return (
     <div className="w-96 shadow p-4">
       <h2 className="flex gap-2 items-center font-bold">
@@ -23,7 +28,10 @@ const ElementSettingSection = ({ selectedEl, clearSelection }: Props) => {
 
       <label className="text-sm">Font Size</label>
 
-      <Select defaultValue={selectedEl?.style?.fontSize || "24px"}>
+      <Select
+        defaultValue={selectedEl?.style?.fontSize || "24px"}
+        onValueChange={(value) => applyStyle("fontSize", value)}
+      >
         <SelectTrigger className="w-full">
           <SelectValue placeholder="Select Size" />
         </SelectTrigger>
@@ -37,6 +45,15 @@ const ElementSettingSection = ({ selectedEl, clearSelection }: Props) => {
           </SelectGroup>
         </SelectContent>
       </Select>
+
+      <label className="text-sm mt-3">Text Color</label>
+      <div>
+        <input
+          type="color"
+          className="w-[40px] h-[40px] rounded-full"
+          onChange={(event) => applyStyle("color", event.target.value)}
+        />
+      </div>
     </div>
   );
 };
